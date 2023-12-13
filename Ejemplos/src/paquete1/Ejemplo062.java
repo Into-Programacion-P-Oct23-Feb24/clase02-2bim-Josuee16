55ac461c7299aff9d9264f5b8f8b6b33a05310f3
Josuee16 <josuexpardoch@gmail.com>
2023-12-13 16:49:31 -0500

    Ejercicio 06

---
+++ Ejemplos/src/paquete1/Ejemplo062.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paquete1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author reroes
+ */
+public class Ejemplo062 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Declarar y generar arreglos en Java
+        Scanner entrada = new Scanner(System.in);
+        int dato;
+        // arreglo de tipo entero
+        int[] arreglo1 = new int[7];
+
+        // Se asigna valores al arreglo, pidiendo valores por teclado
+        // se usa un ciclo repetitivo
+        for (int i = 0; i < arreglo1.length; i++) {
+
+            System.out.println("Ingrese un valor para el arreglo");
+            dato = entrada.nextInt();
+            if ((dato >= 5 && dato <= 9) || (dato == 11 || dato == 13 || dato
+                    == 14)) {
+                /*
+                se crea la condicion para poder generar las restricciones
+                de la tabla
+                 */
+
+            } else { //el else va para que cuando no se cumpla la condicion
+
+                dato = 5;
+                //se cambie automaticamente el valor a 5
+            }
+            // se asigna la variable dato a la posición del arreglo
+            // de acuerdo al valor de i
+            arreglo1[i] = dato;
+
+        }
+
+        // Se hace uso del ciclo repetitivo for para recorrer el arreglo
+        for (int i = 0; i < arreglo1.length; i++) {
+            System.out.printf("Subíndice o Índice %d, con valor %d\n",
+                    i, arreglo1[i]);
+        }
+
+    }
+
+}
